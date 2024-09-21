a7d4cfad614260927c105d8c931168d55471084a
Kushwanth Reddy <pkushwanthreddy@gmail.com>
2024-09-21 09:43:10 -0400

    file rename and code changes for mongodb connection

--- server.js
+++ server.js
@@ -6,12 +6,17 @@ const noteRoutes = require('./routes/noteRoutes');
 dotenv.config();
 const app = express();
 app.use(express.json());
+const mongoURI = process.env.MONGO_URI;
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
-  .catch((err) => console.log(err));
+mongoose.connect(mongoURI)
+  .then(() => {
+    console.log('MongoDB connected');
+  })
+  .catch(err => {
+    console.error('MongoDB connection error:', err);
+  });
 
 app.use('/api', noteRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
